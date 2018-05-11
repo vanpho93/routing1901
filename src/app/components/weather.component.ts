5c164d36c4685a762978ad57747846dab7f0c2a1
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-11 18:53:42 +0700

    onNewCity

--- src/app/components/weather.component.ts
+++ src/app/components/weather.component.ts
@@ -1,25 +1,43 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { WeatherService } from '../services/weather.service';
 
 @Component({
     selector: 'app-weather',
     template: `
         <p>Weather Component</p>
-        <h3>Thoi tiet hien tai cua HaNoi:</h3>
-        <p>Nhiet do 30<sup>o</sup>C</p>
+        <div *ngIf="isLoading">
+            <p>Loading...</p>
+        </div>
+        <div *ngIf="!isLoading && !gotError">
+            <h3>Thoi tiet hien tai cua {{ cityName }}:</h3>
+            <p>Nhiet do {{ temp }}<sup>o</sup>C</p>
+        </div>
+        <div *ngIf="!isLoading && gotError">
+            <h3>Khong the tim thay thanh pho {{ cityName }}</h3>
+        </div>
     `,
     providers: [WeatherService]
 })
 
 export class WeatherComponent {
+    cityName = '';
+    temp = '';
+    gotError = false;
+    isLoading = true;
     constructor(private route: ActivatedRoute, private weatherService: WeatherService) {
-        this.route.paramMap.subscribe(paramMap => {
-            console.log(paramMap.get('cityName'));
-        });
+        this.route.paramMap.subscribe(this.onNewCity.bind(this));
+    }
 
-        this.weatherService.getTempByCityName('12834134716374')
-        .then(temp => console.log(temp))
-        .catch(error => console.log(error.message));
+    onNewCity(paramMap: ParamMap) {
+        this.cityName = paramMap.get('cityName');
+        this.gotError = false;
+        this.isLoading = true;
+        setTimeout(() => {
+            this.weatherService.getTempByCityName(this.cityName)
+            .then(temp => this.temp = temp)
+            .catch(() => this.gotError = true)
+            .then(() => this.isLoading = false);
+        }, 500);
     }
 }
